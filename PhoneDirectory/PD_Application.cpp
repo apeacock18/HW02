f5fd346aa2d1a56216df0ffadf61cf36c2619de5
apeacock18 <andrwpeacock@gmail.com>
2015-09-21 16:28:29 -0700

    Specified directory location.

--- PhoneDirectory/PD_Application.cpp
+++ PhoneDirectory/PD_Application.cpp
@@ -92,7 +92,10 @@ void do_remove_entry(Phone_Directory& the_directory) // Exercise 1.8: please com
 	getline(cin, name);
 	// Complete the rest of this function 
 	string num = the_directory.remove_entry(name);
-	//cout << "Number \"" << num << "\" removed.\n";
+	if (num != "")
+		cout << name << " removed. ( Number: " << num << ")\n";
+	else
+		cout << "Name not found.\n\n";
 }
 
 void do_save(Phone_Directory& the_directory)
